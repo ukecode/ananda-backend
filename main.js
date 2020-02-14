0a950c770e5af98dd63073c3aae56c2faa6757ca
Kaiofprates <kaioprates@hotmail.com>
2020-02-14 20:48:35 -0300

    scraping example

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
-const cron = require('node-cron');
+const puppeteer = require('puppeteer');
 
+//const cron = require('node-cron');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = "{TOKEN}";
+const token = "{TOKEN}"
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -13,25 +14,44 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
-
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+// ----------   Scraping com Puppeteer --------------
+
+async function mandaNude(chatId){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.xvideos.com/');
+      
+    let url  = await page.evaluate(()=>{
+      var images  = document.getElementsByTagName('img')
+       return(images[0].src);
+    })
+
+    bot.sendPhoto(chatId, url);
+    await browser.close();
+}
+
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log(chatId);
 
+
 // --------  
+
+if(msg.text == "#mandanude"){
+
+ mandaNude(chatId);
+}
 
 if(msg.text  == 'Oi'){
     bot.sendMessage(chatId, 'Oiiii sumido, tudo bem?');
-
+ 
 }
 
 if(msg.text  == 'Tudo e vc?'){
@@ -42,6 +62,8 @@ if(msg.text  == 'Tudo e vc?'){
 });
 
 
-cron.schedule('0 6-7 * * * ', () => {
-    bot.sendMessage('823137937', 'Bom dia flor do dia!');
-  });
+//cron.schedule('0 6-7 * * * ', () => {
+//    bot.sendMessage('823137937', 'Bom dia flor do dia!');
+//  });
+
+
